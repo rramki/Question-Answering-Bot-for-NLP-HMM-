712fd8c9e7b222be0d915e3d9e54e98fec7c7a5f
rramki <ramki21in@gmail.com>
2026-03-13 17:11:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import anthropic
 st.title("📚 AI Tutor for NLP")
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+"""
 role = st.sidebar.selectbox("Login as", ["User", "Admin"])
 
 documents = []
@@ -96,3 +97,91 @@ if role == "User":
     else:
 
         st.warning("Admin must upload PDF first.")
+"""
+# Admin login UI:
+role = st.sidebar.selectbox("Login as", ["User", "Admin"])
+
+if role == "Admin":
+
+    username = st.sidebar.text_input("Admin Username")
+    password = st.sidebar.text_input("Admin Password", type="password")
+
+    if username == ADMIN_USER and password == ADMIN_PASS:
+        st.success("Admin logged in")
+        admin_logged = True
+    else:
+        admin_logged = False
+
+#Admin Upload Section
+
+if admin_logged:
+
+    subject = st.selectbox(
+        "Select Subject",
+        ["NLP", "MachineLearning", "CloudComputing"]
+    )
+
+    uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+
+#Create Vector Database
+
+if uploaded_file:
+
+    reader = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    documents = text.split("\n")
+
+    embeddings = model.encode(documents)
+
+    dim = embeddings.shape[1]
+
+    index = faiss.IndexFlatL2(dim)
+
+    index.add(np.array(embeddings))
+
+    folder = f"vectorstore/{subject}"
+
+    os.makedirs(folder, exist_ok=True)
+
+    faiss.write_index(index, f"{folder}/index.faiss")
+
+    np.save(f"{folder}/docs.npy", documents)
+
+    st.success(f"{subject} vector database created!")
+
+#User Query Section
+st.header("Ask Questions")
+
+subject = st.selectbox(
+    "Select Subject",
+    ["NLP", "MachineLearning", "CloudComputing"]
+)
+
+#Load the vector database:
+folder = f"vectorstore/{subject}"
+
+if os.path.exists(f"{folder}/index.faiss"):
+
+    index = faiss.read_index(f"{folder}/index.faiss")
+
+    documents = np.load(f"{folder}/docs.npy", allow_pickle=True)
+
+#Answer Generation
+question = st.text_input("Enter your question")
+
+if question:
+
+    query_vector = model.encode([question])
+
+    distances, ids = index.search(np.array(query_vector), k=2)
+
+    context = ""
+
+    for i in ids[0][:2]:
+        context += documents[i][:700] + "\n"
+
