f172b804722b5be60de2ecd60e42e87c40def9db
rramki <ramki21in@gmail.com>
2026-03-14 09:12:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,33 +29,32 @@ if admin_logged:
     subject = st.selectbox("Select Subject",["nlp", "ml", "cloud"])
     uploaded_file = st.file_uploader("Upload PDF", type="pdf")
 
-if uploaded_file:
-    reader = PdfReader(uploaded_file)
+    if uploaded_file:
+        reader = PdfReader(uploaded_file)
+        text = ""
 
-    text = ""
+        for page in reader.pages:
+            text += page.extract_text()
 
-    for page in reader.pages:
-        text += page.extract_text()
+        documents = text.split("\n")
 
-    documents = text.split("\n")
+        embeddings = model.encode(documents)
 
-    embeddings = model.encode(documents)
+        dim = embeddings.shape[1]
 
-    dim = embeddings.shape[1]
+        index = faiss.IndexFlatL2(dim)
 
-    index = faiss.IndexFlatL2(dim)
+        index.add(np.array(embeddings))
 
-    index.add(np.array(embeddings))
+        folder = f"/vectorstore/{subject}"
 
-    folder = f"/vectorstore/{subject}"
+        os.makedirs(folder, exist_ok=True)
 
-    os.makedirs(folder, exist_ok=True)
+        faiss.write_index(index, f"{folder}/index.faiss")
 
-    faiss.write_index(index, f"{folder}/index.faiss")
+        np.save(f"{folder}/docs.npy", documents)
 
-    np.save(f"{folder}/docs.npy", documents)
-
-    st.success(f"{subject} vector database created!")
+        st.success(f"{subject} vector database created!")
 
 
 #User Interface Page
