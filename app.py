d8932050b3b88766824c789b6aa2f6040407a6b3
rramki <ramki21in@gmail.com>
2026-03-14 09:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,42 +19,42 @@ if role == "Admin":
     username = st.sidebar.text_input("Admin Username")
     password = st.sidebar.text_input("Admin Password", type="password")
 
-if username == ADMIN_USER and password == ADMIN_PASS:
-    st.success("Admin logged in")
-    admin_logged = True
-else:
-    admin_logged = False
+    if username == ADMIN_USER and password == ADMIN_PASS:
+        st.success("Admin logged in")
+        admin_logged = True
+    else:
+        admin_logged = False
 
-if admin_logged:
-    subject = st.selectbox("Select Subject",["nlp", "ml", "cloud"])
-    uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+    if admin_logged:
+        subject = st.selectbox("Select Subject",["nlp", "ml", "cloud"])
+        uploaded_file = st.file_uploader("Upload PDF", type="pdf")
 
-    if uploaded_file:
-        reader = PdfReader(uploaded_file)
-        text = ""
+        if uploaded_file:
+            reader = PdfReader(uploaded_file)
+            text = ""
 
-        for page in reader.pages:
-            text += page.extract_text()
+            for page in reader.pages:
+                text += page.extract_text()
 
-        documents = text.split("\n")
+            documents = text.split("\n")
 
-        embeddings = model.encode(documents)
+            embeddings = model.encode(documents)
 
-        dim = embeddings.shape[1]
+            dim = embeddings.shape[1]
 
-        index = faiss.IndexFlatL2(dim)
+            index = faiss.IndexFlatL2(dim)
 
-        index.add(np.array(embeddings))
+            index.add(np.array(embeddings))
 
-        folder = f"/vectorstore/{subject}"
+            folder = f"/vectorstore/{subject}"
 
-        os.makedirs(folder, exist_ok=True)
+            os.makedirs(folder, exist_ok=True)
 
-        faiss.write_index(index, f"{folder}/index.faiss")
+            faiss.write_index(index, f"{folder}/index.faiss")
 
-        np.save(f"{folder}/docs.npy", documents)
+            np.save(f"{folder}/docs.npy", documents)
 
-        st.success(f"{subject} vector database created!")
+            st.success(f"{subject} vector database created!")
 
 
 #Studnet  Interface Page
